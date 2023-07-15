176d095d93e51791f8d4d392158068e2c7dfac35
Constantin Cernean <ccernean@yahoo.com.com>
2023-07-15 14:14:36 -0400

    Added working API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,1 +1,13 @@
 import axios from 'axios';
+
+const fetchWeatherData = async (location) => {
+  try {
+    const response = await axios.get(`https://www.ncei.noaa.gov/cdo-web/api/v2/locations=${location}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.data.error);
+  }
+};
+
+export { fetchWeatherData };
+
