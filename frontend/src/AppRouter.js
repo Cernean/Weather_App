0a5b1de8c58d1a1cb2f59de6eee1cd5cc74e952d
Constantin Cernean <ccernean@yahoo.com.com>
2023-07-15 11:11:54 -0400

    Created AppRouter

--- frontend/src/AppRouter.js
+++ frontend/src/AppRouter.js
@@ -1,2 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Home from './pages/Home';
+import UserProfile from './pages/UserProfile';
+import WeatherDetails from './pages/WeatherDetails';
 
+const AppRouter = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path= "/profile" component={UserProfile} />
+                <Route exact path= "/weather/:location" component={WeatherDetails} />          
+            </Switch>
+        </Router>
+    );
+};
+
+export default AppRouter;
