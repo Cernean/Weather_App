9ada3f2704d67b192d37527ef7637bea5d8e9179
Constantin Cernean <ccernean@yahoo.com.com>
2023-07-15 10:20:03 -0400

    Created the weather details page

---
+++ frontend/src/pages/WeatherDetails.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WeatherDetails = ({ location }) => {
+    return (
+        <div>
+            <h1> Weather Details for {location}</h1>
+        </div>
+    );
+};
+
+export default WeatherDetails;
