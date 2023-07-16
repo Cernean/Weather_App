6e1e94d7c3c0fddc7d2313ecac595af33c21597c
Constantin Cernean <ccernean@yahoo.com.com>
2023-07-16 11:47:58 -0400

    Created Weather Details

---
+++ frontend/src/pages/WeatherDetails.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchWeatherData } from '../services/api';
+
+const WeatherDetails = () => {
+  const { location } = useParams();
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    fetchWeatherData(location)
+      .then((data) => {
+        setWeatherData(data);
+      })
+      .catch((error) => {
+        console.log('Error fetching weather data:', error);
+      });
+  }, [location]);
+
+  return (
+    <div>
+      {weatherData ? (
+        <div>
+          <h2>{weatherData.location}</h2>
+          <p>Temperature: {weatherData.temperature}°F</p>
+          <p>Description: {weatherData.description}</p>
+        </div>
+      ) : (
+        <p>Loading weather data...</p>
+      )}
+    </div>
+  );
+};
+
+export default WeatherDetails;
+
